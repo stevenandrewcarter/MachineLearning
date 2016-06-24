6dfff1dedb098e8fed887d9d807d82b6828e1aec
Steven Carter <stonemason@gmail.com>
2016-06-24 16:37:05 +0200

    * Added Type Providers

---
+++ TypeProviders/RProvider.fsx
@@ -0,0 +1,65 @@
+﻿#I @"..\packages\"
+#r @"FSharp.Data.2.3.1\lib\net40\FSharp.Data.dll"
+
+open FSharp.Data
+
+let wb = WorldBankData.GetDataContext ()
+let countries = wb.Countries
+
+let pop2000 = [ for c in countries -> c.Indicators.``Population, total``.[2000]]
+let pop2010 = [ for c in countries -> c.Indicators.``Population, total``.[2010]]
+
+#r @"R.NET.Community.1.6.5\lib\net40\RDotNet.dll"
+#r @"RProvider.1.1.20\lib\net40\RProvider.Runtime.dll"
+#r @"RProvider.1.1.20\lib\net40\RProvider.dll"
+
+open RProvider
+open RProvider.``base``
+open RProvider.graphics
+
+// Retrieve an (F#) list of country surfaces
+let surface = [ for c in countries -> c.Indicators.``Surface area (sq. km)``.[2010]]
+// Produce summary statistics
+R.summary(surface) |> R.print
+// Plot the corresponding histogram
+R.hist(surface)
+// Plot the transform
+R.hist(surface |> R.log)
+R.hist(surface |> List.map log)
+
+// Plot population against surface
+R.plot(surface, pop2010)
+R.plot(surface |> R.log, pop2010 |> R.log)
+
+// create data frame, the hard way
+
+let pollution = [ for c in countries -> c.Indicators.``CO2 emissions (kt)``.[2000]]
+let electricity = [ for c in countries -> c.Indicators.``Access to electricity (% of population)``.[2000]]
+
+let rdf =
+    [ "Pop2000", box pop2000
+      "Pop2010", box pop2010
+      "Surface", box surface
+      "Pollution", box pollution
+      "Electricity", box electricity ]
+    |> namedParams
+    |> R.data_frame
+
+// Scatterplot of all features
+rdf |> R.plot
+
+// Summary of all features
+rdf |> R.summary |> R.print
+
+#r @"Deedle.1.2.5\lib\net40\Deedle.dll"
+open Deedle
+
+let series1 = series [ "Alpha", 1.; "Bravo", 2.; "Delta", 4.]
+let series2 = series [ "Bravo", 20.; "Charlie", 30.; "Delta", 40.]
+let toyFrame = frame [ "First", series1; "Second", series2]
+series1 |> Stats.sum 
+toyFrame |> Stats.mean
+toyFrame?Second |> Stats.mean
+
+toyFrame?New <- toyFrame?First + toyFrame?Second
+toyFrame |> Stats.mean
---
+++ TypeProviders/Script.fsx
@@ -0,0 +1,42 @@
+﻿#I @"..\packages\"
+#r @"FSharp.Data.2.3.1\lib\net40\FSharp.Data.dll"
+
+open FSharp.Data
+
+type Questions = JsonProvider<"""https://api.stackexchange.com/2.2/questions?site=stackoverflow""">
+let csQuestions = """https://api.stackexchange.com/2.2/questions?site=stackoverflow&tagged=C%23"""
+Questions.Load(csQuestions).Items |> Seq.iter (fun q -> printfn "%s" q.Title)
+
+let questionQuery = """https://api.stackexchange.com/2.2/questions?site=stackoverflow"""
+let tagged tags query = 
+    let joinedTags = tags |> String.concat ";"
+    sprintf "%s&tagged=%s" query joinedTags
+
+let page  p query = sprintf "%s&page=%i" query p
+
+let pageSize s query = sprintf "%s&pagesize=%i" query s
+
+let extractQuestions (query:string) = Questions.Load(query).Items
+
+let ``C#`` = "C%23"
+let ``F#`` = "F%23"
+let fsSample = questionQuery |> tagged[``F#``] |> pageSize 100 |> extractQuestions
+let csSample = questionQuery |> tagged[``C#``] |> pageSize 100 |> extractQuestions
+
+let anaylzeTags (qs:Questions.Item seq) =
+    qs 
+    |> Seq.collect (fun question -> question.Tags)
+    |> Seq.countBy id
+    |> Seq.filter (fun (_, count) -> count > 2)
+    |> Seq.sortBy (fun (_, count) -> count)
+    |> Seq.iter (fun (tag,count) -> printfn "%s, %i" tag count)
+
+anaylzeTags fsSample
+anaylzeTags csSample
+
+let wb = WorldBankData.GetDataContext()
+wb.Countries.Japan.CapitalCity
+
+let countries = wb.Countries
+let pop2000 = [ for c in countries -> c.Indicators.``Population, total``.[2000]]
+let pop2010 = [ for c in countries -> c.Indicators.``Population, total``.[2010]]
