4c5cf27ce17f858ba39fc85990956eb396b60fe9
Steven Carter <stonemason@gmail.com>
2016-06-29 23:01:25 +0200

    * Added in BikesAndMan

---
+++ BikesAndMen/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace BikesAndMen.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("BikesAndMen")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("BikesAndMen")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("9074efdb-069e-4f36-bb58-5d95ce9e78c7")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ BikesAndMen/BikesAndMan.fsx
@@ -0,0 +1,62 @@
+﻿#I @"..\packages\"
+#r @"FSharp.Data.2.3.1\lib\net40\FSharp.Data.dll"
+
+open FSharp.Data
+
+type Data = CsvProvider<"..\Data\day.csv">
+let dataset = Data.Load("..\Data\day.csv")
+let data = dataset.Rows
+
+#load @"FSharp.Charting.0.90.14\FSharp.Charting.fsx"
+open FSharp.Charting
+
+let all = Chart.Line [ for obs in data -> obs.Cnt ]
+
+let windowedExample = 
+    [1 .. 10 ]
+    |> Seq.windowed 3
+    |> Seq.toList
+
+let ma n (series:float seq) = 
+    series
+    |> Seq.windowed n
+    |> Seq.map (fun xs -> xs |> Seq.average)
+    |> Seq.toList
+
+let count = 
+    data |> Seq.map (fun x -> float x.Cnt)
+
+Chart.Combine [
+    Chart.Line count
+    Chart.Line (ma 7 count)
+    Chart.Line (ma 30 count)
+]
+
+let baseline =
+    let avg = data |> Seq.averageBy (fun x -> float x.Cnt)
+    data |> Seq.averageBy (fun x -> abs (float x.Cnt - avg))
+
+type Obs = Data.Row
+
+let model (theta0, theta1) (obs:Obs) =
+    theta0 + theta1 * (float obs.Instant)
+
+let model0 = model (4504., 0.)
+let model1 = model (6000., -4.5)
+
+Chart.Combine [
+    Chart.Line count
+    Chart.Line [ for obs in data -> model0 obs]
+    Chart.Line [ for obs in data -> model1 obs]
+]
+
+type Model = Obs -> float
+
+let cost (data:Obs seq) (m:Model) =
+    data
+    |> Seq.sumBy (fun x -> pown (float x.Cnt - m x) 2)
+    |> sqrt
+
+let overallCost = cost data
+overallCost model0 |> printfn "Cost model0: %.0f"
+overallCost model1 |> printfn "Cost model1: %.0f"
